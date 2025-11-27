544adba9234007ac03140853e9c10f20690d8dd4
jcaugant <43166742+jcaugant@users.noreply.github.com>
2025-11-27 14:23:59 +0100

    Update code_stats.py

--- code_stats.py
+++ code_stats.py
@@ -24,8 +24,15 @@ def evolution_depot(col, annee_d, annee_f) :
     st.line_chart(df[['Nombre de codes']])
     st.dataframe(df, width = 500)
 
-
-st.write("Sélectionnez l'indicateur que vous souhaitez afficher")
-st.selectbox('Sélectionnez l\'indicateur que vous souhaitez afficher',
+analyse = st.selectbox('Sélectionnez l\'indicateur que vous souhaitez afficher',
     ('Choix','Evolution des dépôts de codes dans un portail ou une collection', 'Répartition des dépôts de codes par langage'))
 
+if analyse == 'Evolution des dépôts de codes dans un portail ou une collection' :
+    portail = st.sidebar.text_input("Entrez l'acronyme du portail ou de la collection à analyser", "")
+    year_s = st.sidebar.selectbox(
+    'Sélectionnez une année de départ',
+    (2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024))
+    year_e = st.sidebar.selectbox(
+    'Sélectionnez une année de fin',
+    (2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025))
+    evolution_depot(portail,year_s,year_e)
