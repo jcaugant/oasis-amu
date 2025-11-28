a560876466fe3a517001d197de28497b45b73c30
jcaugant <43166742+jcaugant@users.noreply.github.com>
2025-11-28 10:34:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,11 @@ pages = {
         st.Page("code_stats.py", title="Evolution des dépôts"),
         st.Page("code_lang.py", title="Répartition des dépôts par langage"),
     ],
+    "Dépôts": [
+        st.Page("col_stats.py", title="Evolution des dépôts"),
+        st.Page("col_chart.py", title=" Répartition des dépôts par type"),
+        st.Page("project_chart.py", title=" Répartition des projets ANR et ERC"),
+        st.Page("detect_false.py", title="Détection des doublons"),
 }
 
 pg = st.navigation(pages)
