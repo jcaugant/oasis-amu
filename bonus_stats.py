00e5fe77d094c34f5e9da7aee6a1b166a3e0c770
jcaugant <43166742+jcaugant@users.noreply.github.com>
2025-12-01 12:12:23 +0100

    Update bonus_stats.py

--- bonus_stats.py
+++ bonus_stats.py
@@ -7,12 +7,12 @@ if portail == "" :
   st.markdown(''' Veuillez renseigner le code de votre collection ou portail comme indiqué dans [Aurehal](https://aurehal.archives-ouvertes.fr/structure/index)''')
 link = st.sidebar.checkbox('Ne pas compter les notices avec lien Open-Access')
 liste_hal = []
-url_total = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&fq=publicationDateY_i:2024&fq=structId_i:{portail}&wt=json"
-url_file = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&fq=publicationDateY_i:2024&fq=structId_i:{portail}&wt=json&fq=submitType_s:file"
+url_total = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&fq=publicationDateY_i:2025&fq=structId_i:{portail}&wt=json"
+url_file = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&fq=publicationDateY_i:2025&fq=structId_i:{portail}&wt=json&fq=submitType_s:file"
 if link:
-  url_notice = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&rows=10000&fq=publicationDateY_i:2024&fq=structId_i:{portail}&wt=json&fq=(submitType_s:notice%20NOT%20openAccess_bool:(true))&fl=halId_s"
+  url_notice = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&rows=10000&fq=publicationDateY_i:2025&fq=structId_i:{portail}&wt=json&fq=(submitType_s:notice%20NOT%20openAccess_bool:(true))&fl=halId_s"
 else :
-  url_notice = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&rows=10000&fq=publicationDateY_i:2024&fq=structId_i:{portail}&wt=json&fq=submitType_s:notice&fl=halId_s"
+  url_notice = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&rows=10000&fq=publicationDateY_i:2025&fq=structId_i:{portail}&wt=json&fq=submitType_s:notice&fl=halId_s"
 req_file, req_notice, req_total = rq.get(url_file), rq.get(url_notice), rq.get(url_total)
 req_file, req_notice, req_total = req_file.json(), req_notice.json(), req_total.json()
 try :
@@ -21,9 +21,9 @@ try :
     halid = req_notice['response']['docs'][i]['halId_s']
     halid= "https://amu.hal.science/"+halid
     liste_hal.append(halid)
-  st.write(f"Pour l'année 2024, votre structure a déposé {file} documents avec texte intégral et {notice} notices")
-  # st.write(f"Votre pourcentage de publications avec texte intégral déposées dans HAL pour l'année 2024 est donc de : ", int((file/total)*100), "%")
-  st.write("Votre pourcentage de publications avec texte intégral déposées dans HAL pour l'année 2024 est donc de : ")
+  st.write(f"Pour l'année 2025, votre structure a déposé {file} documents avec texte intégral et {notice} notices")
+  # st.write(f"Votre pourcentage de publications avec texte intégral déposées dans HAL pour l'année 2025 est donc de : ", int((file/total)*100), "%")
+  st.write("Votre pourcentage de publications avec texte intégral déposées dans HAL pour l'année 2025 est donc de : ")
   pourcent = int((file/total)*100)
   st.subheader(f"{pourcent}%")
   st.write("Vous trouverez la liste ci-dessous des notices HAL pour lesquelles le texte intégral est manquant")
