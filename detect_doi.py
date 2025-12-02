eebcc0e616bf00c47be236a3ba16b8d6fc673d74
jcaugant <43166742+jcaugant@users.noreply.github.com>
2025-12-02 14:17:40 +0100

    Update detect_doi.py

--- detect_doi.py
+++ detect_doi.py
@@ -1,1 +1,36 @@
+import requests as rq
+import pandas as pd
+import streamlit as st
 
+fichier = st.file_uploader("Ajoutez un fichier à analyser")
+if fichier is not None:
+  df = pd.read_csv(fichier)
+  list_doi, list_result, list_hal = [], [], []
+  for i in range(0, len(df['doi'])):
+    doi = df['doi'][i]
+    list_doi.append(doi)
+    try :
+      url = f"https://api.archives-ouvertes.fr/search/?q=*:*&wt=json&fl=submitType_s,halId_s&fq=doiId_s:{doi}"
+      req = rq.get(url)
+      req = req.json()
+      result = req['response']['docs'][0]['submitType_s']
+      hal_id = req['response']['docs'][0]['halId_s']
+      list_result.append(result)
+      list_hal.append(hal_id)
+    except :
+      result = "Pas dans HAL"
+      hal_id = " "
+      list_result.append(result)
+      list_hal.append(hal_id)
+                
+
+  df_hal = pd.DataFrame(
+    {
+    "DOI": list_doi,
+    "Type de dépôts": list_result,
+    "Identifiant HAL" : list_hal,
+    }
+    )
+    st.dataframe(df_hal)
+  else :
+    st.write("Ajoutez un fichier au format .csv comprenant la liste de vos DOI dans une seule colonne appelée doi")
