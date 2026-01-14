73814c2835d5a88c8e9f1139a131802a3a84e128
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-01-14 11:42:35 +0100

    Enhance dataframe display with three columns
    Added columns to display three dataframes side by side.

--- struct_clean.py
+++ struct_clean.py
@@ -30,7 +30,16 @@ def struct_clean(col):
        "Structures": unknown,
    }
     )
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.dataframe(df_valid, width = 100)
+
+with col2:
+    st.dataframe(df_old, width = 100)
+
+with col3:
+    st.dataframe(df_unknown, width = 100)
   
 portail = st.text_input("Entrez le numéro d'identifiant du portail ou de la collection à analyser", "")
 struct_clean(portail)
