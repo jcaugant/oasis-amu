4b03319e302401472576d8ea71a6105982cdb240
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-01-14 10:42:59 +0100

    Refactor struct_clean function to extract names

--- struct_clean.py
+++ struct_clean.py
@@ -6,9 +6,15 @@ def struct_clean(col):
   valid, old, unknown = [],[],[]
   url = f"https://api.archives-ouvertes.fr/ref/structure/?q=parentDocid_i:{col}&fl=name_s&wt=json&indent=true&group=true&group.field=valid_s&group.limit=300"
   req = rq.get(url).json()
-  valid = len(req['grouped']['valid_s']['groups'][0]['doclist']['docs'])
-  old = len(req['grouped']['valid_s']['groups'][1]['doclist']['docs'])
-  unknown = len(req['grouped']['valid_s']['groups'][2]['doclist']['docs'])
+  for i in range(len(req['grouped']['valid_s']['groups'][0]['doclist']['docs'])):
+    value = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][i]['names_s']
+    valid.append(value)
+  for j in range(len(req['grouped']['valid_s']['groups'][1]['doclist']['docs'])):
+    value = req['grouped']['valid_s']['groups'][1]['doclist']['docs'][i]['names_s']
+    old.append(value)
+  for k in range(len(req['grouped']['valid_s']['groups'][2]['doclist']['docs'])):
+    value = req['grouped']['valid_s']['groups'][2]['doclist']['docs'][i]['names_s']
+    unknown.append(value)
   st.write(valid, old, unknown)
 
 
