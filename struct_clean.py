418376166a2e7751c4c2121cdc784abcdf3cbf3e
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-01-14 10:58:57 +0100

    Refactor struct_clean to handle multiple documents
    Iterate over the documents and append to valid list.

--- struct_clean.py
+++ struct_clean.py
@@ -6,8 +6,12 @@ def struct_clean(col):
   valid, old, unknown = [],[],[]
   url = f"https://api.archives-ouvertes.fr/ref/structure/?q=parentDocid_i:{col}&fl=name_s&wt=json&indent=true&group=true&group.field=valid_s&group.limit=300"
   req = rq.get(url).json()
-  val = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][0]
-  st.write(val)
+  for i in range(0, len(req['grouped']['valid_s']['groups'][0]['doclist']['docs'])):
+    val = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][0]
+    valid.append(val)
+    st.write(valid)
+    
+  
 
 portail = st.text_input("Entrez le numéro d'identifiant du portail ou de la collection à analyser", "")
 struct_clean(portail)
