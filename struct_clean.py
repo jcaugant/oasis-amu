cd9a794ae96796eb5a359f92ab09e47c7e709816
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-01-14 10:50:46 +0100

    Clean up struct_clean function
    Removed unused variable assignment and output statement.

--- struct_clean.py
+++ struct_clean.py
@@ -6,11 +6,6 @@ def struct_clean(col):
   valid, old, unknown = [],[],[]
   url = f"https://api.archives-ouvertes.fr/ref/structure/?q=parentDocid_i:{col}&fl=name_s&wt=json&indent=true&group=true&group.field=valid_s&group.limit=300"
   req = rq.get(url).json()
-  valid = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][0]['names_s']
-  st.write(valid)
-
-
-
 
 portail = st.text_input("Entrez le numéro d'identifiant du portail ou de la collection à analyser", "")
 struct_clean(portail)
