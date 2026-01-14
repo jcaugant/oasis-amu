e6d30b2a4067754f5209ac4977938850bbee32ff
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-01-14 10:51:54 +0100

    Fix struct_clean function to retrieve valid names

--- struct_clean.py
+++ struct_clean.py
@@ -6,6 +6,8 @@ def struct_clean(col):
   valid, old, unknown = [],[],[]
   url = f"https://api.archives-ouvertes.fr/ref/structure/?q=parentDocid_i:{col}&fl=name_s&wt=json&indent=true&group=true&group.field=valid_s&group.limit=300"
   req = rq.get(url).json()
+  val = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][i]['names_s']
+  st.write(val)
 
 portail = st.text_input("Entrez le numéro d'identifiant du portail ou de la collection à analyser", "")
 struct_clean(portail)
