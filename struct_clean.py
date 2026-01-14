25fceb3de5ba7940d58b3869b4096ac7ffdeb466
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-01-14 10:53:19 +0100

    Modify struct_clean to retrieve all valid documents
    Updated the extraction of 'val' to include all documents instead of just the first one.

--- struct_clean.py
+++ struct_clean.py
@@ -6,7 +6,7 @@ def struct_clean(col):
   valid, old, unknown = [],[],[]
   url = f"https://api.archives-ouvertes.fr/ref/structure/?q=parentDocid_i:{col}&fl=name_s&wt=json&indent=true&group=true&group.field=valid_s&group.limit=300"
   req = rq.get(url).json()
-  val = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][0]['names_s']
+  val = req['grouped']['valid_s']['groups'][0]['doclist']
   st.write(val)
 
 portail = st.text_input("Entrez le numéro d'identifiant du portail ou de la collection à analyser", "")
