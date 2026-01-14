35aa29a5f3965fa110670278eab50192d04e0975
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-01-14 10:50:18 +0100

    Refactor struct_clean to simplify valid names extraction
    Simplified the extraction of valid names from the API response by directly accessing the first group's docs instead of iterating through all groups.

--- struct_clean.py
+++ struct_clean.py
@@ -6,16 +6,8 @@ def struct_clean(col):
   valid, old, unknown = [],[],[]
   url = f"https://api.archives-ouvertes.fr/ref/structure/?q=parentDocid_i:{col}&fl=name_s&wt=json&indent=true&group=true&group.field=valid_s&group.limit=300"
   req = rq.get(url).json()
-  for i in range(0, len(req['grouped']['valid_s']['groups'][0]['doclist']['docs'])):
-    value = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][i]['names_s']
-    valid.append(value)
-  for j in range(0, len(req['grouped']['valid_s']['groups'][1]['doclist']['docs'])):
-    value = req['grouped']['valid_s']['groups'][1]['doclist']['docs'][j]['names_s']
-    old.append(value)
-  for k in range(0, len(req['grouped']['valid_s']['groups'][2]['doclist']['docs'])):
-    value = req['grouped']['valid_s']['groups'][2]['doclist']['docs'][k]['names_s']
-    unknown.append(value)
-  st.write(valid, old, unknown)
+  valid = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][0]['names_s']
+  st.write(valid)
 
 
 
