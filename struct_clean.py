ac7adbcdb00e965813f8a6dabbc7e4cfd1f1d589
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-01-14 11:00:20 +0100

    Correct document indexing in struct_clean.py
    Fix indexing in the loop to append valid documents.

--- struct_clean.py
+++ struct_clean.py
@@ -7,11 +7,9 @@ def struct_clean(col):
   url = f"https://api.archives-ouvertes.fr/ref/structure/?q=parentDocid_i:{col}&fl=name_s&wt=json&indent=true&group=true&group.field=valid_s&group.limit=300"
   req = rq.get(url).json()
   for i in range(0, len(req['grouped']['valid_s']['groups'][0]['doclist']['docs'])):
-    val = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][0]
+    val = req['grouped']['valid_s']['groups'][0]['doclist']['docs'][i]
     valid.append(val)
-    st.write(valid)
-    
-  
 
 portail = st.text_input("Entrez le numéro d'identifiant du portail ou de la collection à analyser", "")
 struct_clean(portail)
+st.write(valid)
