f49dbf50062508e29824922d726d756d4a836000
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-03-31 10:54:44 +0200

    Update list_authors.py

--- list_authors.py
+++ list_authors.py
@@ -3,8 +3,8 @@ import pandas as pd
 import requests as rq
 from datetime import datetime
 
-def listing(struct,year) :
-  url = f"https://api.archives-ouvertes.fr/search/?q=*:*&rows=0&wt=json&facet=true&facet.query=structHasAuthIdHal_fs&facet.field=structHasAuthIdHal_fs&facet.prefix={struct}_FacetSep_&facet.mincount=1&facet.limit=8000&fq=publicationDateY_i:{year}"
+def listing(struct,year_s, year_e) :
+  url = f"https://api.archives-ouvertes.fr/search/?q=*:*&rows=0&wt=json&facet=true&facet.query=structHasAuthIdHal_fs&facet.field=structHasAuthIdHal_fs&facet.prefix={struct}_FacetSep_&facet.mincount=1&facet.limit=8000&fq=publicationDateY_i:[{year_s} TO {year_e}]"
   data = rq.get(url).json()
   facets = data["facet_counts"]["facet_fields"]["structHasAuthIdHal_fs"]
   rows = []
@@ -29,10 +29,13 @@ def listing(struct,year) :
   st.dataframe(df, width = 1500) 
 
 struct = st.text_input("Entrez le numéro HAL de votre structure ", "")
-year = st.selectbox(
-'Sélectionnez une année de recherche',
+year_s = st.selectbox(
+'Sélectionnez une année de début de recherche',
 (2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026))
-listing(struct,year)
+year_e = st.selectbox(
+'Sélectionnez une année de fin de recherche',
+(2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026))
+listing(struct,year_s, year_e)
 
 
 
