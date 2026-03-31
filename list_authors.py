028b7c1fc786badd57812176477627945798befc
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-03-31 10:41:45 +0200

    Implement author listing function in list_authors.py
    Added a function to list authors based on structure and year.

--- list_authors.py
+++ list_authors.py
@@ -3,6 +3,36 @@ import pandas as pd
 import requests as rq
 from datetime import datetime
 
+def listing_auteurs(struct,year) :
+  url = f"https://api.archives-ouvertes.fr/search/?q=*:*&rows=0&wt=json&facet=true&facet.query=structHasAuthIdHal_fs&facet.field=structHasAuthIdHal_fs&facet.prefix={struct}_FacetSep_&facet.mincount=1&facet.limit=8000&fq=publicationDateY_i:{year}"
+  data = requests.get(url).json()
+  facets = data["facet_counts"]["facet_fields"]["structHasAuthIdHal_fs"]
+  rows = []
+
+  for i in range(0, len(facets), 2):
+    value = facets[i]
+    count = facets[i + 1]
+
+    parts = (
+        value.replace("_JoinSep_", "_FacetSep_")
+             .split("_FacetSep_")
+    )
+
+    rows.append({
+        "struct_id": parts[0],
+        "structure": parts[1],
+        "idhal": parts[2],
+        "auteur": parts[3],
+        "nb_documents": count
+    })
+  df = pd.DataFrame(rows)
+
+struct = st.text_input("Entrez le numéro HAL de votre structure ", "")
+year = st.selectbox(
+'Sélectionnez une année de recherche',
+(2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026))
+listing(struct,year)
+
 
 
 
