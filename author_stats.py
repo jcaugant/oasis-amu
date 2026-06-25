dfb68f48a2952f705dc46a2e078e6968d1f6600e
jcaugant <43166742+jcaugant@users.noreply.github.com>
2026-06-25 09:56:39 +0200

    Update author_stats.py

--- author_stats.py
+++ author_stats.py
@@ -3,8 +3,9 @@ import requests as rq
 import streamlit as st
 
 def stats_scrap(id) :
-  api = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&rows=5000&wt=json&fq=authIdHal_s:{id}&fl=uri_s"
+  api = f"https://api.archives-ouvertes.fr/search/?q=*%3A*&rows=5000&wt=json&fq=authIdHal_s:{id}&fl=uri_s,publicationDate_s,submittedDate_s"
   req = rq.get(api).json()
+  submit_date, publish_date, consult, download = [],[],[],[]
   st.write(req)
 
 idhal = st.text_input("Entrez l'IdHal de l'auteur", "")
